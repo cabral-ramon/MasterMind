af80e1cff4184bcfbaceaa9445fe6506fbb7ef24
Ramon <rrcm16@gmail.com>
2018-09-29 11:03:34 -0400

    Shrink key size

--- client/src/components/Feedback/Key.js
+++ client/src/components/Feedback/Key.js
@@ -1,26 +1,27 @@
 import React from "react";
+import "./key.css";
 
 const Key = () => (
-  <div>
-    <h6>Key:</h6>
-    <div className="row mb-1">
-      <div className="col-md-2">
+  <div className="key-container">
+    <h6 style={{marginRight: "20px"}}>Key:</h6>
+    <div className="row mb-1 key-item">
+      <div className="col-md-2 key-icon-container">
         <i className="fas fa-check" style={{ color: "green" }} />
       </div>
       <div className="col-md-8">
         The number is correct and in the right location.
       </div>
     </div>
-    <div className="row mb-1">
-      <div className="col-md-2">
+    <div className="row mb-1 key-item">
+      <div className="col-md-2 key-icon-container">
         <i className="far fa-circle" style={{ color: "blue" }} />
       </div>
       <div className="col-md-8">
         The number is part of the code but it is in the wrong place.
       </div>
     </div>
-    <div className="row mb-1">
-      <div className="col-md-2">
+    <div className="row mb-1 key-item">
+      <div className="col-md-2 key-icon-container">
         <i className="fas fa-times" style={{ color: "red" }} />
       </div>
       <div className="col-md-8">The number is not part of the code.</div>
